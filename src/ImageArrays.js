95418257ab6799c71b571c5a1f62da0b6979a86c
JacobMJones <mrjacobtaiwan@gmail.com>
2018-08-23 11:43:00 -0400

    refactored innertile, cleaned out state and created imageArrays module to handle paths

---
+++ src/ImageArrays.js
@@ -0,0 +1,24 @@
+export default {
+       frontImage: [
+        "topleft.png",
+        "topmid.png",
+        "topright.png",
+        "midleft.png",
+        "midmid.png",
+        "midright.png",
+        "bottomleft.png",
+        "bottommid.png",
+        "bottomright.png"
+      ],
+      backImage: [   
+            "topleftback6.png",
+            "topmidback.png",
+            "toprightback.png",
+            "midleftback.png",
+            "mmback.png",
+            "midrightback.png",
+            "bottomleftback.png",
+            "bottommidback.png",
+            "bottomrightback.png",      
+      ],
+}
--- src/InnerTile.js
+++ src/InnerTile.js
@@ -1,53 +1,13 @@
-import React, { Component } from "react";
-import ReactSVG from "react-svg";
-import TileScaleAnimations from "./TileScaleAnimations.js";
-
+import React, { Component } from "react"
+import ReactSVG from "react-svg"
+import TileScaleAnimations from "./TileScaleAnimations.js"
+import ImageArrays from "./ImageArrays.js"
 
 class InnerTile extends Component {
   constructor(props) {
     super(props);
     this.state = {
       flipped: false,
-      titles: [
-        "Completed Projects",
-        "Current Projects",
-        "Fragments",
-        "To-Do Blog",
-        "Learning Blog",
-        "Big To-Do",
-        "FAQ",
-        "Resume",
-        "Look Into My Eyes"
-      ],
-      myBack:[
-        "topleftback6.png",
-        "topmidback.png",
-        "toprightback.png",
-        "midleftback.png",
-        "mmback.png",
-        "midrightback.png",
-        "bottomleftback.png",
-        "bottommidback.png",
-        "bottomrightback.png",
-      ],
-
-      cardSVGs: [
-        "completed-project.svg",
-        "current-project.svg",
-        "fragments2.svg",
-        "todo.svg"
-      ],
-      myFace: [
-        "topleft.png",
-        "topmid.png",
-        "topright.png",
-        "midleft.png",
-        "midmid.png",
-        "midright.png",
-        "bottomleft.png",
-        "bottommid.png",
-        "bottomright.png"
-      ]
     };
   }
   componentDidUpdate(nextProps) {
@@ -69,26 +29,22 @@ class InnerTile extends Component {
       <img
         className="face"
         style={{ position: "absolute", width: "100%", height: "80%" }}
-        src={this.state.myFace[this.props.id]}
+        src={ImageArrays.frontImage[this.props.id]}
       />
       <div  
       className="back"
         style={{ position: "absolute", width: "100%", height: "80%" }}
-        src={this.state.myFace[this.props.id]}
+        src={ImageArrays.backImage[this.props.id]}
       > 
 
       <div style={{ width:'100%', height:"100%"}}>
-      <img  style={{position: 'relative', textAlign:'center', width:'100%', height:"100%"}} src={this.state.myBack[this.props.id]}></img>
+      <img  style={{position: 'relative', textAlign:'center', width:'100%', height:"100%"}} src={ImageArrays.backImage[this.props.id]}></img>
       </div>
       {/* <div style={{zIndex:'100', fontSize:'125%', position: 'relative', textAlign:'center', top:'-50%'}}>
      {this.state.titles[this.props.id]} 
      
       </div> */}
-      <div style={{position:'relative', left: '25%', top:'-20%', width:'50%'}}>  
-      
-      {/* <ReactSVG  path={this.state.cardSVGs[this.props.id]}></ReactSVG>  */}
-      
-      </div>
+    
      
       </div>
       </div>
